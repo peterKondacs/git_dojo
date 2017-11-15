547939322245f70f6e97ad3fefaa855a945e1a85
Agnes Gal <galagnes277@gmail.com>
2017-11-15 09:41:09 +0100

    Bad joke added.

--- gittie.py
+++ gittie.py
@@ -1,2 +1,11 @@
 def say_hello ():
     print("Hello, I'm Gittie!")
+
+def joke_telling():
+    answer = input("would you like a joke?")
+    if answer == "Yes":
+        print("Egy fickó a taxisofőrhöz: '\n'- Mennyiért visz el Kecskemétig? '\n'- Nem a távolságtól, hanem az időtől függ - feleli a sofőr.'\n' - Mondjuk akkor, ha esik?")
+    else:
+        print("Then have a boring day.")
+
+joke_telling()
